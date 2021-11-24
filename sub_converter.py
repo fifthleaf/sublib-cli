aef7566b41bdb88b57af08552c18c6af770bfdba
Dawid Jach <jach.developer@protonmail.com>
2021-11-24 14:54:07 +0100

    Add log system

--- sub_converter.py
+++ sub_converter.py
@@ -2,17 +2,17 @@ import os
 import sys
 import chardet as chdt
 import argparse as arps
+import logging as lg
 import sublib as sbl
 
 
 def parser():
-    """
-    Parses given arguments
-    """
+    """Parses given arguments"""
     arg_parser = arps.ArgumentParser(
-        usage="sub_converter.py [-h] path form_to",
+        usage="sub_converter [--help] [path form_to [--log <file>]]",
         description="Offline Sub Converter allows you to change subtitle formats using the command console.",
-        epilog="Supported formats: mpl, srt, sub, tmp"
+        epilog="Supported formats: mpl, srt, sub, tmp",
+        formatter_class=lambda prog: arps.HelpFormatter(prog, max_help_position=52)
     )
     arg_parser.add_argument(
         "path",
@@ -27,18 +27,50 @@ def parser():
         metavar="form_to",
         help="Target format"
     )
+    arg_parser.add_argument(
+        "-l", "--log",
+        type=str,
+        nargs="?",
+        const=True,
+        metavar="log",
+        help="Enable logging. Optionally takes a file"
+    )
     args = arg_parser.parse_args()
-    return (args.path, args.form_to)
+    return (args.path, args.form_to, args.log)
 
 
-if __name__ == "__main__":
+def set_logger(path_file, lvl):
+    """Set logger ready to use"""
+    path_file = os.path.normpath(path_file)
+    path_file = os.path.abspath(path_file)
+    lg.basicConfig(
+        filename=path_file, filemode="wt", encoding="utf-8",
+        format="[%(levelname)s][%(asctime)s][%(module)s.%(funcName)s] %(message)s",
+        level=lvl
+    )
+    lg.info("Set up logger")
+    return lg.getLogger(__name__)
 
-    path, form_to = parser()
+
+def main():
+
+    path, form_to, log_file = parser()
     path = os.path.normpath(path)
     path = os.path.abspath(path)
 
+    if log_file is None:
+        log_file = f"{os.path.splitext(__file__)[0]}.log"
+        logger = set_logger(log_file, lg.CRITICAL)
+    elif log_file is True:
+        log_file = f"{os.path.splitext(__file__)[0]}.log"
+        logger = set_logger(log_file, lg.INFO)
+    else:
+        logger = set_logger(log_file, lg.INFO)
+
     if os.path.exists(path) is False:
+        logger.critical("Entered path does not exists")
         sys.exit("Entered path does not exists")
+    logger.info("Correct path: " + str(path))
 
     form_from, encodings, lines_list = [], [], []
 
@@ -46,28 +78,38 @@ if __name__ == "__main__":
 
         with open(path, "rb") as f:
             encoding = chdt.detect(f.read())["encoding"]
+            logger.info("Encoding detected: " + str(encoding))
         form = sbl.detect(path, encoding)
         if form is False:
+            logger.critical("Cannot convert this file")
             sys.exit("Cannot convert this file")
+        logger.info("Correct file: " + str(path))
         files = [path]
         form_from.append(form)
         encodings.append(encoding)
+        logger.info("Ready file: " + str(path))
 
     elif os.path.isdir(path):
 
         files = list(os.walk(path))[0][2]
+        logger.info("File list: " + str(files))
         files = [file.replace(file, path + "\\" + file) for file in files]
         for file in files:
             with open(file, "rb") as f:
                 encoding = chdt.detect(f.read())["encoding"]
+                logger.info("Encoding detected: " + str(encoding))
             form = sbl.detect(file, encoding)
             if form is False:
                 files.remove(file)
+                logger.info("Remove unsupported file: " + str(file))
             else:
                 form_from.append(form)
                 encodings.append(encoding)
+                logger.info("Ready file: " + str(file))
         if len(form_from) <= 0:
+            logger.critical("No valid files in the path")
             sys.exit("No valid files in the path")
+        logger.info("All files ready")
 
     for form, *param in zip(form_from, files, encodings):
 
@@ -80,6 +122,8 @@ if __name__ == "__main__":
         elif form == "tmp":
             new_file = sbl.from_tmp(*param)
         lines_list.append(new_file)
+
+    logger.info("All files converted to general format")
 
     if form_to == "mpl":
 
@@ -100,3 +144,18 @@ if __name__ == "__main__":
 
         for parameters in zip(files, lines_list, encodings):
             sbl.to_tmp(*parameters)
+
+    logger.info("All files writed")
+
+    lg.shutdown()
+    if os.path.getsize(log_file) == 0:
+        os.remove(log_file)
+
+
+if __name__ == "__main__":
+
+    try:
+        main()
+    except Exception:
+        print("An unknown error has occurred:")
+        print(sys.exc_info())
--- sublib.py
+++ sublib.py
@@ -1,11 +1,13 @@
 import re
 import datetime as dt
+import logging as lg
+
+
+logger = lg.getLogger(__name__)
 
 
 def detect(path_file, encoding):
-    """
-    Detect subtitle format
-    """
+    """Detect subtitle format"""
     mpl_reg = "\\[[0-9]+\\]\\[[0-9]+\\] .*\n"                                                           # [START][STOP] TEXT\n
     srt_reg = "[0-9]+\n[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}\n"   # NUM\nSTART --> STOP\n
     sub_reg = "{[0-9]+}{[0-9]+}.*\n"                                                                    # {START}{STOP}TEXT\n
@@ -22,6 +24,7 @@ def detect(path_file, encoding):
         result = "tmp"
     else:
         result = False
+    logger.info("Detected format: " + str(result))
     return result
 
 
@@ -29,38 +32,35 @@ def detect(path_file, encoding):
 
 
 def from_mpl(path_file, encoding):
-    """
-    Convert MPL to general list
-    """
+    """Convert MPL to general list"""
     with open(path_file, "rt", encoding=encoding, errors="ignore") as file:
         lines = [line.split("]", 2) for line in (line.rstrip("\n") for line in file)]
     for line in lines:
         line[0] = dt.timedelta(seconds=round(float(line[0].replace("[", "")) / 10.0, 1))   # MPL use as time: sec * 10
         line[1] = dt.timedelta(seconds=round(float(line[1].replace("[", "")) / 10.0, 1))
         line[2] = line[2].lstrip()
+    logger.info(f"Make general format from MPL file: {str(path_file)}")
     return lines
 
 
 def to_mpl(path_file, lines, encoding):
-    """
-    Convert general list to MPL
-    """
+    """Convert general list to MPL"""
     for line in lines:
         line[0] = round(line[0].total_seconds() * 10)   # MPL use as time: sec * 10
         line[1] = round(line[1].total_seconds() * 10)
     formated_lines = [f"[{line[0]}][{line[1]}] {line[2]}" for line in lines]
+    logger.info(f"Make MPL from general format: {str(path_file)}")
     with open(path_file[:path_file.rfind(".")] + ".txt", "wt", encoding=encoding, errors="ignore") as file:
         for line in formated_lines:
             file.write(line + "\n")
+        logger.info(f"Write MPL to new .txt file: {str(path_file)}")
 
 
 # SRT Functions
 
 
 def from_srt(path_file, encoding):
-    """
-    Convert SRT to general list
-    """
+    """Convert SRT to general list"""
     with open(path_file, "rt", encoding=encoding, errors="ignore") as file:
         lines, temp = [], []
         for line in file:
@@ -79,13 +79,12 @@ def from_srt(path_file, encoding):
             line[2] = line[2].replace(style, "")
         for style in re.findall(r"<.*>", line[2]):
             line[2] = line[2].replace(style, "")
+    logger.info(f"Make general format from SRT file: {str(path_file)}")
     return lines
 
 
 def to_srt(path_file, lines, encoding):
-    """
-    Convert general list to SRT
-    """
+    """Convert general list to SRT"""
     for line in lines:
         line[0] = str(line[0])
         line[1] = str(line[1])
@@ -101,18 +100,18 @@ def to_srt(path_file, lines, encoding):
         line[1] = line[1][:len(line[1]) - 3]
         line[2] = line[2].replace("|", "\n")
     formated_lines = [f"{num}\n{line[0]} --> {line[1]}\n{line[2]}\n" for num, line in enumerate(lines, 1)]
+    logger.info(f"Make SRT from general format: {str(path_file)}")
     with open(path_file[:path_file.rfind(".")] + ".srt", "wt", encoding=encoding, errors="ignore") as file:
         for line in formated_lines:
             file.write(line + "\n")
+        logger.info(f"Write SRT to new .srt file: {str(path_file)}")
 
 
 # SUB Functions
 
 
 def from_sub(path_file, encoding):
-    """
-    Convert SUB to general list
-    """
+    """Convert SUB to general list"""
     with open(path_file, "rt", encoding=encoding, errors="ignore") as file:
         lines = [line.split("}", 2) for line in (line.rstrip("\n") for line in file)]
     for line in lines:
@@ -120,29 +119,28 @@ def from_sub(path_file, encoding):
         line[1] = dt.timedelta(seconds=round(float(line[1].replace("{", "")) / 23.976, 3))
         for style in re.findall(r"{.*}", line[2]):
             line[2] = line[2].replace(style, "")
+    logger.info(f"Make general format from SUB file: {str(path_file)}")
     return lines
 
 
 def to_sub(path_file, lines, encoding):
-    """
-    Convert general list to SUB
-    """
+    """Convert general list to SUB"""
     for line in lines:
         line[0] = round(line[0].total_seconds() * 23.976)   # SUB use frames as time
         line[1] = round(line[1].total_seconds() * 23.976)
     formated_lines = [f"{{{line[0]}}}{{{line[1]}}}{line[2]}" for line in lines]
+    logger.info(f"Make SUB from general format: {str(path_file)}")
     with open(path_file[:path_file.rfind(".")] + ".sub", "wt", encoding=encoding, errors="ignore") as file:
         for line in formated_lines:
             file.write(line + "\n")
+        logger.info(f"Write SUB to new .sub file: {str(path_file)}")
 
 
 # TMP Functions
 
 
 def from_tmp(path_file, encoding):
-    """
-    Convert TMP to general list
-    """
+    """Convert TMP to general list"""
     with open(path_file, "rt", encoding=encoding, errors="ignore") as file:
         lines = [line.split(":", 3) for line in (line.rstrip("\n") for line in file)]
         lines = [[f"{line[0]}:{line[1]}:{line[2]}", f"{line[0]}:{line[1]}:{line[2]}", line[3]] for line in lines]
@@ -151,13 +149,12 @@ def from_tmp(path_file, encoding):
         line[1] = dt.datetime.strptime(line[1], "%H:%M:%S")
         line[0] = dt.timedelta(hours=line[0].hour, minutes=line[0].minute, seconds=line[0].second, microseconds=line[0].microsecond)
         line[1] = dt.timedelta(hours=line[1].hour, minutes=line[1].minute, seconds=line[1].second + 1, microseconds=line[1].microsecond)
+    logger.info(f"Make general format from TMP file: {str(path_file)}")
     return lines
 
 
 def to_tmp(path_file, lines, encoding):
-    """
-    Convert general list to TMP
-    """
+    """Convert general list to TMP"""
     for line in lines:
         line[0] = str(line[0])
         if len(line[0]) == 7:
@@ -166,6 +163,8 @@ def to_tmp(path_file, lines, encoding):
         line[0] = line[0].strftime("%H:%M:%S.%f")
         line[0] = line[0][:len(line[0]) - 7]
     formated_lines = [f"{line[0]}:{line[2]}" for line in lines]
+    logger.info(f"Make TMP from general format: {str(path_file)}")
     with open(path_file[:path_file.rfind(".")] + ".txt", "wt", encoding=encoding, errors="ignore") as file:
         for line in formated_lines:
             file.write(line + "\n")
+        logger.info(f"Write TMP to new .txt file: {str(path_file)}")
