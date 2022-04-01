939a77b965677079a1e269825725c9ca1a403543
Dawid Jach <jach.developer@protonmail.com>
2022-04-01 15:19:22 +0200

    Add CLI arguments parser

--- sublib-cli/sublib-cli.py
+++ sublib-cli/sublib-cli.py
@@ -5,3 +5,39 @@ import argparse
 
 import sublib
 import chardet
+
+
+def parser():
+    arg_parser = argparse.ArgumentParser(
+        usage=f"{os.path.basename(__file__)} "
+              f"[--help] [path form_to [--log <file>]]",
+        description="CLI implementation of Sublib package.",
+        epilog="Supported formats: mpl, srt, sub, tmp",
+        formatter_class=lambda prog: argparse.HelpFormatter(
+            prog,
+            max_help_position=52
+        )
+    )
+    arg_parser.add_argument(
+        "path",
+        type=str,
+        metavar="path",
+        help="Directory or single file to convert"
+    )
+    arg_parser.add_argument(
+        "form_to",
+        type=str,
+        choices=["mpl", "srt", "sub", "tmp"],
+        metavar="form_to",
+        help="Target format"
+    )
+    arg_parser.add_argument(
+        "-l", "--log",
+        type=str,
+        nargs="?",
+        const=True,
+        metavar="log",
+        help="Enable logging. Optionally takes a file"
+    )
+    args = arg_parser.parse_args()
+    return (args.path, args.form_to, args.log)
