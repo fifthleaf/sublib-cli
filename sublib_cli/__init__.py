a20a5a108e6eb9b8a50a06f5949a9832c46b1f1d
Dawid Jach <jach.developer@protonmail.com>
2022-04-19 21:05:51 +0200

    Add imports section and __version__ variable

---
+++ sublib_cli/__init__.py
@@ -0,0 +1,5 @@
+from sublib_cli.cli import parser, set_logger, find_files
+from sublib_cli.cli import detect_encoding, get_subtitle, get_new_path
+from sublib_cli.cli import write_file, end, main
+
+__version__ = "1.2.0"
